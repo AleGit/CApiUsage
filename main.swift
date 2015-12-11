eaa6cbf1746bc67e1261a884c35f2ff0add63111
Alexander Maringele <nyaya@maringele.at>
2015-12-11 17:11:30 +0100

    main with comments

--- main.swift
+++ main.swift
@@ -1,3 +1,8 @@
+/* CYicesSample demonstrates the usage of The Yices SMT Solver's in Swift.
+
+
+// print version of installed Yices
 version()
 
+// demonstrate quantifier free predicate logic
 demo()
