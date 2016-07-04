0ecbe82e2364869284b19172e96cd323d85b5985
Alexander Maringele <amaringele@am-x240.uibk.ac.at>
2016-07-04 11:28:29 +0200

    Update to Swift 3 Preview

--- Demo.swift
+++ Demo.swift
@@ -2,46 +2,45 @@ import CYices
 
 /// Converts and prints C-String `yices_version`
 func version() {
-    
+
     print("======================")             // print separator line before all output
     defer { print("======================") }   // print separator line after all output
-    
-    let version = String.fromCString(yices_version) ?? "not"
-    
+
+    let version = String(validatingUTF8:yices_version) ?? "not"
+
     print("Yices",version,"installed.")
-    
+
 }
 
-private func status(context:COpaquePointer, term: term_t) {
-    
+private func status(context:OpaquePointer, term: term_t) {
+
     defer { print("-----------------------------------------------") } // print separator line after all output
-    
+
     guard let string = String(term: term) else { return }
     print("assert", string)
-    
+
     yices_assert_formula(context, term)
-    
+
     switch (yices_check_context(context, nil)) {
     case STATUS_SAT:
         print("Satisfiable");
-        
+
         // build the model and print it
-        let mdl = yices_get_model(context, 1)
-        guard mdl != nil else { return }
+        guard let mdl = yices_get_model(context, 1) else { return }
         defer { yices_free_model(mdl) }
-        
+
         guard let model_string = String(model:mdl) else { return }
         print(model_string)
         break;
-        
+
     case STATUS_UNSAT:
         print("Unsatisfiable");
         break;
-        
+
     case STATUS_UNKNOWN:
         print("Status is unknown");
         break;
-        
+
     case STATUS_IDLE:
         fallthrough
     case STATUS_SEARCHING:
@@ -60,52 +59,52 @@ private func status(context:COpaquePointer, term: term_t) {
 /// Constructs thre clauses:
 /// - a tautology
 /// - a positive literal clause `p(f(a,b),a,b)`
-/// - a negative literal clause 
+/// - a negative literal clause
 /// an checks satisfiability.
 func demo() {
     yices_init()
     defer { yices_exit() }
-    
-    let context = yices_new_context(nil)
+
+    guard let context = yices_new_context(nil) else { return }
     defer { yices_free_context(context) }
-    
+
     let bool_tau = yices_bool_type()
     let free_tau = yices_int_type()
-    
+
     // constant 'a'
     let a = yices_new_uninterpreted_term(free_tau)
     yices_set_term_name(a, "a")
-    
+
     // constant 'b'
     let b = yices_new_uninterpreted_term(free_tau)
     yices_set_term_name(b, "b")
-    
+
     // tpye '(free,free)->free'
-    let f_domain = [type_t](count:2, repeatedValue:free_tau)
+    let f_domain = [type_t](repeating:free_tau, count:2)
     let f_tau = yices_function_type(UInt32(f_domain.count), f_domain, free_tau)
     let f = yices_new_uninterpreted_term(f_tau)
     yices_set_term_name(f, "f")
-    
+
     // function 'f(a,b)'
     var args = [a,b]
     let fab = yices_application(f,UInt32(args.count), args)
-    
+
     // type '(free,free,free)->bool'
     let p_domain = [ free_tau, free_tau, free_tau ]
     let p_tau = yices_function_type(UInt32(p_domain.count), p_domain, bool_tau)
     let p = yices_new_uninterpreted_term(p_tau)
     yices_set_term_name(p, "p")
-    
+
     // predicate 'p(fab,a,b)'
     args = [fab,a,b]
     let pfab = yices_application(p,UInt32(args.count), args)
-    
+
     // 'NOT p(fab,a,b)'
     let npfab = yices_not(pfab)
     // 'p(fab,a,b) OR NOT p(fab,a,b)'
     let tautology = yices_or2(pfab,npfab)
-    
-    status(context, term:tautology)
-    status(context, term:pfab)
-    status(context, term:npfab)
+
+    status(context:context, term:tautology)
+    status(context:context, term:pfab)
+    status(context:context, term:npfab)
 }
